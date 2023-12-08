0ecc1f9583ca80c5f534be7b3ebc6d678d973b84
Amice Baptiste <amice.bap@gmail.com>
2023-12-08 18:15:12 +0100

    Balanced strings tested
    Co-authored-by: UN-L <UN-L@users.noreply.github.com>

--- code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
+++ code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
@@ -7,5 +7,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilsTest {
 
+    // La chaine peut être vide => vrai
+    @Test
+    void testEmptyString() {
+        assertTrue(isBalanced(""));
+    }
+
+    // Fermetures correspondantes aux ouvertures => vrai
+    @Test
+    void testBalancedString() {
+        assertTrue(isBalanced("({[]})"));
+    }
+
+    // Fermetures non correspondantes aux ouvertures par nombre d'apparition => faux
+    @Test
+    void testUnbalancedString() {
+        assertFalse(isBalanced("(()"));
+    }
+
+    // Fermerture non correspondante par ordre d'apparition => faux
+    @Test
+    void testUnbalancedString2() {
+        assertFalse(isBalanced("([)]"));
+    }
+
 
 }
